5f4b23d42f223c38733c8d761757fc79c1616bfb
VadymBezsmertnyi <vados199368@gmail.com>
2021-08-23 10:59:54 +0300

    Fix share....

--- js/share.js
+++ js/share.js
@@ -1,15 +1,15 @@
 var share = document.querySelector('#share p');
 
 document.addEventListener('scroll', function () {
-    if (window.scrollY > 401 && window.scrollY < 3100) {
+    if (window.scrollY > 261 && window.scrollY < 3653) {
         share.style.color = 'black';
     }
 
-    else if(window.scrollY > 0 && window.scrollY < 400){
+    else if(window.scrollY > 0 && window.scrollY < 260){
         share.style.color = 'white';        
     }
 
-    else if(window.scrollY > 3101 && window.scrollY < 3750){
+    else if(window.scrollY > 3654 && window.scrollY < 3950){
         console.log('gfgfgf');
         share.style.color = 'white';        
     }
